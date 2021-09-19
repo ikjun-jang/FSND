86974cf8fbda3c89b2243b053d2fbebd23cabd98
unknown <jyj34690@gmail.com>
2021-09-19 22:35:15 +0800

    added node modules folder to .gitignore and changed database path setting to use environment variables

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+from flask.globals import session
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -14,12 +15,19 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "student", "student", "localhost:5432", self.database_name
-        )
 
-        setup_db(self.app, self.database_path)
+        """ 
+        @EDIT: Database credentials handled
+        using the dynamic environment variables 
+        """
+        self.DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+        self.DB_USER = os.getenv('DB_USER', 'postgres')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
+        self.DB_NAME = os.getenv('DB_NAME', 'trivia_test')  
+        self.DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+            self.DB_USER, self.DB_PASSWORD, self.DB_HOST, self.DB_NAME)
+
+        setup_db(self.app, self.DB_PATH)
 
         # to test successful result
         self.new_question = {
@@ -28,7 +36,7 @@ class TriviaTestCase(unittest.TestCase):
             "difficulty": 2,
             "category": 3
         }
-
+        
         # to test unsuccessful result
         self.invalild_question = {
             "question": "What is the largest country?",
@@ -108,15 +116,30 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(len(data["questions"]), 0)
         self.assertTrue(data["current_category"])
 
+    def test_create_new_question(self):
+        res = self.client().post("/questions", json=self.new_question)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_404_sent_invalid_question_info(self):
+        res = self.client().post("/books", json=self.invalild_question)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_delete_question(self):
-        res = self.client().delete('/questions/4')
+        res = self.client().delete('/questions/24')
         data = json.loads(res.data)
 
-        question = Question.query.filter(Question.id == 4).one_or_none()
+        question = Question.query.filter(Question.id == 24).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'], 4)
+        self.assertEqual(data['deleted'], 24)
         self.assertEqual(question, None)
 
     def test_422_if_question_does_not_exist(self):
@@ -142,21 +165,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], "resource not found")
-
-    def test_create_new_question(self):
-        res = self.client().post("/questions", json=self.new_question)
-        data = json.loads(res.data)
-        
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-
-    def test_404_sent_invalid_question_info(self):
-        res = self.client().post("/books", json=self.invalild_question)
-        data = json.loads(res.data)
-        
-        self.assertEqual(res.status_code, 404)
-        self.assertEqual(data['success'], False)
-        self.assertEqual(data['message'], 'resource not found')
 
     def test_get_next_question(self):
         res = self.client().post("/quizzes", 
