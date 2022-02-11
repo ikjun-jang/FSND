01e0eb788ee40910531a0e8ac3736f888968d8fd
ikjun-jang <jyj34690@gmail.com>
2022-02-11 23:07:08 +0900

    initial commit

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+export DATABASE_URL="postgresql://postgres:1701@localhost:5432/agency"
+export DATABASE_TEST_URL="postgresql://postgres:1701@localhost:5432/agency_test"
+export AUTH0_DOMAIN="fsnd3469.us.auth0.com"
+export API_AUDIENCE="agency"
+export ALGORITHMS="RS256"
+export CLIENT_ID="xF3XrLq6kJVBcZbl46cSdpewX8BsP8q7"
+echo "setup.sh script executed successfully!"
